61ae19eeda95ab130404feafd5876c9dcabb86f3
mahdoosh1 <mahdiyadiilavi1@gmail.com>
2025-10-30 17:15:02 +0330

    reduce

--- test.py
+++ test.py
@@ -13,53 +13,9 @@ with open("example.py", "w") as file:
     text = gen.generate()
     file.write(text)
 
-# import example
-# with open("example.bmp","rb") as file:
-#     data = file.read()
-# data = example.parseFile(data, 0, {})[0]['pixels']['rows'][50]
-# pprint(data)
-# import json
-
-# def valid(obj, key, value):
-#     if not value:
-#         return False
-#     if key == "pos":
-#         return False
-#     if key == "name":
-#         if isinstance(value, str):
-#             if not isinstance(obj, ast_.Identifier):
-#                 return False
-#     return True
-
-# def keyrepr(obj, key, value):
-#     output = key
-#     if not isinstance(value, str):
-#         output = f"{key}={value.__class__.__name__}"
-#     if isinstance(getattr(value, "name", None), str):
-#         if not isinstance(value, ast_.Identifier):
-#             if isinstance(value, str):
-#                 output += f"({value})"
-#             else:
-#                 output += f"({value.name})"
-#     return output
-
-# def ast_to_json(obj):
-#     if isinstance(obj, (list, tuple)):
-#         iterator = enumerate(obj)
-#     elif hasattr(obj, "__dict__"):
-#         iterator = vars(obj).items()
-#     elif isinstance(obj, (str, int, NoneType)):
-#         return obj
-#     else:
-#         return obj
-#     return {
-#             keyrepr(obj, k, v):ast_to_json(v) for k, v in iterator if valid(obj, k, v)
-#         }
-
-# #print(parsed.items[-1])
-
-# with open("ast.json", "w") as f:
-#     json.dump(ast_to_json(parsed), f, indent=2)
-
-# from os import system
-# system("open ast.json")
+import example
+with open("example.bmp","rb") as file:
+    data = file.read()
+data = example.parseFile(data, 0, {})[0]['pixels']['rows'][50]
+pprint(data)
+import json
