7f427c5a290080a06db0a4f52456d4bdf977f161
mahdoosh1 <mahdiyadiilavi1@gmail.com>
2025-11-05 19:11:30 +0330

    fix

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ from sys import argv
 file_name = argv[1] if len(argv) > 1 else "example.spp"
 result = argv[2] if len(argv) > 2 else file_name.replace(".spp",".py")
 test_data = argv[3] if len(argv) > 3 else file_name.replace(".spp",".bin")
+if len(argv) <= 1:
+    test_data = "example.bmp"
 
 with open(file_name) as file:
     code = file.read()
