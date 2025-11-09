08fb5acb8923e9cb14ad11c2e0649335be206504
mahdoosh1 <mahdiyadiilavi1@gmail.com>
2025-11-09 21:27:12 +0330

    move examples

--- test.py
+++ test.py
@@ -3,11 +3,9 @@ from pprint import pprint
 from types import NoneType
 from sys import argv
 
-file_name = argv[1] if len(argv) > 1 else "example.spp"
+file_name = argv[1] if len(argv) > 1 else "test/example.spp"
 result = argv[2] if len(argv) > 2 else None
 test_data = argv[3] if len(argv) > 3 else None
-if len(argv) <= 1:
-    test_data = "example.bmp"
 
 with open(file_name) as file:
     code = file.read()
